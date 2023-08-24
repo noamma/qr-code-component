82ab635222b70e3605735abbddfd4eda81ce12f4
noamma <noamma@markodt.com>
2023-08-24 12:17:14 +0000

    changes to style and html structure

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import  qrimg from "./img/image-qr-code.png"
 const App = () => {
     
     return (
-        <>
-        <div className="wrapper">
-            <div className="img-wrapper">
-                {qrimg}
+        <div className="container">
+            <div className="card">
+                <div className="img-wrapper">
+                    <img src={qrimg}/>
+                </div>
+                <div className="par-wrappper">
+                    <p className="desc">Improve your front-end skills by building projects</p>
+                    <p className="inst">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+                </div>
             </div>
-            <p className="desc">Improve your front-end skills by building projects</p>
-            <p className="inst">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
         </div>
-        </>
     );
 }
 
